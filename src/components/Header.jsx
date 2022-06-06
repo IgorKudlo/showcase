c8fd5fcc1482e2957f4f1c128fc6e545ce746f33
igor.kudlo <igor.kudlo@spiralscout.com>
2022-06-06 11:19:07 +0300

    update structure, add Header / Footer

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+function Header() {
+    return (
+        <nav className="light-blue darken-4">
+            <div className="nav-wrapper">
+                <a href="#" className="brand-logo">Showcase</a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li><a href="https://github.com/IgorKudlo/showcase">Repo</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Header
