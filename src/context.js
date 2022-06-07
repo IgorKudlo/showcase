f080542ef16c451be58754a6ae4f3dfe995cce5a
igor.kudlo <igor.kudlo@spiralscout.com>
2022-06-07 12:03:55 +0300

    create useReducer

--- src/context.js
+++ src/context.js
@@ -1,10 +1,36 @@
-import {createContext} from 'react';
+import {createContext, useReducer} from 'react';
+import {reducer} from './reducer';
 
 export const ShopContext = createContext(undefined);
 
+const initialState = {
+    goods: [],
+    loading: true,
+    order: [],
+    isBasketShow: false,
+    alertName: ''
+}
+
 export const ContextProvider = ({children}) => {
-    const value = {
-        example: 'test'
+    const [value, dispatch] = useReducer(reducer, initialState);
+
+    value.closeAlert = () => {
+        dispatch({ type: 'CLOSE_ALERT' });
+    }
+    value.addToBasket = (item) => {
+        dispatch({ type: 'ADD_TO_BASKET', payload: item});
+    }
+    value.removeFromBasket = (itemId) => {
+        dispatch({ type: 'REMOVE_FROM_BASKET', payload: {id: itemId} });
+    }
+    value.incQuantity = (itemId) => {
+        dispatch({ type: 'INCREMENT_QUANTITY', payload: {id: itemId} });
+    }
+    value.decQuantity = (itemId) => {
+        dispatch({ type: 'DECREMENT_QUANTITY', payload: {id: itemId} });
+    }
+    value.handleBasketShow = () => {
+        dispatch({ type: 'TOGGLE_BASKET' });
     }
 
     return (
